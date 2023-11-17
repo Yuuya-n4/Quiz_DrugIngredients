22bf942a5dbbb23216c98fafe7b688a76083ac66
Yuuya-n4 <yuuya9ny@gmail.com>
2023-11-17 17:41:51 +0900

    トップページ機能の追加

---
+++ app/helpers/quiz_sets_helper.rb
@@ -0,0 +1,2 @@
+module QuizSetsHelper
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+QuizSet.create(title: 'サンプルクイズセット1', description: '説明文1')
+QuizSet.create(title: 'サンプルクイズセット2', description: '説明文2')
