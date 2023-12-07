40d18168ade17230ae740b77a7097789294cad67
Yuuya-n4 <yuuya9ny@gmail.com>
2023-12-07 17:42:14 +0900

    クイズのランダム出題と、不正行為対策

--- app/controllers/quiz_sets_controller.rb
+++ app/controllers/quiz_sets_controller.rb
@@ -7,18 +7,21 @@ class QuizSetsController < ApplicationController
   end
 
   def show
-    @quizzes = @quiz_set.quizzes.order(:id)
-    @first_quiz = @quizzes.first
   end
 
   def start_quiz
     @quiz_set = QuizSet.find(params[:id])
-    @first_quiz = @quiz_set.quizzes.order(:id).first
+    quiz_ids = @quiz_set.quizzes.pluck(:id).shuffle
+    session[:quiz_ids] = quiz_ids
+    session[:original_quiz_ids] = quiz_ids.dup
+    session[:answered_quiz_ids] = []
 
-    if @first_quiz.present?
-      @score = current_user.scores.create(quiz_set: @quiz_set, correct_answer: 0)
-      session[:score_id] = @score.id
-      redirect_to quiz_set_quiz_path(@quiz_set, @first_quiz)
+    @score = current_user.scores.create(quiz_set: @quiz_set, correct_answer: 0)
+    session[:score_id] = @score.id
+
+    if session[:quiz_ids].present?
+      next_quiz_id = session[:quiz_ids].shift
+      redirect_to quiz_set_quiz_path(@quiz_set, next_quiz_id)
     else
       redirect_to quiz_sets_path, alert: 'クイズが見つかりません。'
     end
@@ -31,6 +34,7 @@ class QuizSetsController < ApplicationController
   private
 
   def set_quiz_set
-    @quiz_set = QuizSet.find(params[:id])
+    @quiz_set = QuizSet.find_by(id: params[:id])
+    redirect_to(quiz_sets_path, alert: '指定されたクイズセットが見つかりません。') unless @quiz_set
   end
 end
