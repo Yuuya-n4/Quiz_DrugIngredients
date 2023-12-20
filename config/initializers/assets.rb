7bf621db3edc18651f5b573ac0572f27696136c4
Yuuya-n4 <yuuya9ny@gmail.com>
2023-12-20 13:31:55 +0900

    css変数で画像を読めるよう変更

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
 
-Rails.application.config.assets.precompile += %w( application.css rails_admin.css autocomplete_controller.js )
+Rails.application.config.assets.precompile += %w( application.css rails_admin.css toppage.css autocomplete_controller.js )
