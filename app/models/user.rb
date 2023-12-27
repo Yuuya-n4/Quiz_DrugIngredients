331a727b35d3ee94a072b0282a1ba4b6781b075a
Yuuya-n4 <127364780+Yuuya-n4@users.noreply.github.com>
2023-12-27 18:49:13 +0900

    苦手克服クイズを改良
    説明などの文言や表を改善

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,19 +35,47 @@ class User < ApplicationRecord
     end
   end
 
-  def non_weak_quizzes_count
-    user_quiz_performance_summaries.reject(&:weak_subject?).count
-  end
-
   def weak_quizzes_count
     user_quiz_performance_summaries.select do |summary|
-      summary.weak_subject? && summary.consecutive_correct_answers < 2
+      summary.weak_subject? && !summary.recently_improved? && summary.answered_twice_or_more?
     end.count
   end
 
   def mastered_quizzes_count
     user_quiz_performance_summaries.select do |summary|
-      summary.weak_subject? && summary.consecutive_correct_answers >= 2
+      summary.weak_subject? && summary.recently_improved? && summary.answered_twice_or_more?
     end.count
+  end
+
+  def non_weak_quizzes_count
+    user_quiz_performance_summaries.select do |summary|
+      summary.answered_twice_or_more? && summary.accuracy > 80
+    end.count
+  end
+
+  def quiz_set_stats(quiz_set)
+    total_quizzes = quiz_set.quizzes.count
+    summaries = user_quiz_performance_summaries.joins(:quiz)
+                  .where(quizzes: { quiz_set_id: quiz_set.id })
+
+    answered_twice_or_more_summaries = summaries.select(&:answered_twice_or_more?)
+
+    # 苦手な問題（克服した問題を除外）
+    weak_quizzes = answered_twice_or_more_summaries.select(&:weak_subject?)
+    weak_quizzes_count = weak_quizzes.reject(&:recently_improved?).count
+
+    # 克服した問題
+    mastered_quizzes_count = weak_quizzes.select(&:recently_improved?).count
+
+    # 定着した問題
+    non_weak_quizzes_count = answered_twice_or_more_summaries.reject(&:weak_subject?).count
+
+    {
+      total_quizzes: total_quizzes,
+      answered_twice_or_more_count: answered_twice_or_more_summaries.count,
+      weak_quizzes_count: weak_quizzes_count,
+      mastered_quizzes_count: mastered_quizzes_count,
+      non_weak_quizzes_count: non_weak_quizzes_count
+    }
   end
 end
