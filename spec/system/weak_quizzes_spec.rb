45ca08a63eb9551eae0294fc28cea11a68491f53
Yuuya-n4 <yuuya9ny@gmail.com>
2024-04-03 23:55:51 +0900

    苦手克服クイズの修正

--- spec/system/weak_quizzes_spec.rb
+++ spec/system/weak_quizzes_spec.rb
@@ -25,16 +25,17 @@ RSpec.describe 'WeakQuizzes', type: :system do
             click_link '次へ'
           elsif page.has_link?('スコアを見る')
             click_link 'スコアを見る'
+            expect(page).to have_content('あなたのスコア:')
+            expect(page).to have_content(quiz_set.title)
+            expect(page).to have_content('正解数: 10 / 10 問')
           end
         end
-        expect(page).to have_content('あなたのスコア:')
-        expect(page).to have_content(quiz_set.title)
-        expect(page).to have_content('正解数: 10 / 10 問')
       end
     end
   
     visit start_weak_quizzes_path
-    expect(page).to have_content '定着したクイズの数: 30問'
+    expect(page).to have_content('定着したクイズの数:')
+    expect(page).to have_content('30問')
   end
 
   it 'ユーザーが全てのクイズに2回不正解した後、苦手クイズを開くと、「苦手と判定されたクイズの数: 30問」と表示される' do
@@ -61,7 +62,8 @@ RSpec.describe 'WeakQuizzes', type: :system do
     end
   
     visit start_weak_quizzes_path
-    expect(page).to have_content '苦手と判定されたクイズの数: 30問'
+    expect(page).to have_content('苦手と判定されたクイズの数:')
+    expect(page).to have_content('30問')
   end
 
   it 'ユーザーが全てのクイズに2回不正解した後、全てのクイズに2回連続で正解し苦手クイズを開くと、「苦手を克服したクイズの数:  30問」と表示される2' do
@@ -88,7 +90,8 @@ RSpec.describe 'WeakQuizzes', type: :system do
     end
 
     visit start_weak_quizzes_path
-    expect(page).to have_content '苦手と判定されたクイズの数: 30問'
+    expect(page).to have_content('苦手と判定されたクイズの数:')
+    expect(page).to have_content('30問')
 
     2.times do
       @quiz_sets.each do |quiz_set|
@@ -113,7 +116,8 @@ RSpec.describe 'WeakQuizzes', type: :system do
     end
   
     visit start_weak_quizzes_path
-    expect(page).to have_content '苦手を克服したクイズの数: 30問'
+    expect(page).to have_content('苦手を克服したクイズの数:')
+    expect(page).to have_content('30問')
   end
 
   it 'ユーザーが1つのクイズセットに対して2回挑戦し全て不正解した後、苦手クイズに2回挑み全て正解すると、「苦手を克服したクイズの数: 30問」と表示される' do
@@ -135,7 +139,8 @@ RSpec.describe 'WeakQuizzes', type: :system do
     end
 
     visit start_weak_quizzes_path
-    expect(page).to have_content '苦手と判定されたクイズの数: 10問'
+    expect(page).to have_content('苦手と判定されたクイズの数:')
+    expect(page).to have_content('10問')
 
     2.times do
         visit start_weak_quizzes_path
@@ -156,6 +161,7 @@ RSpec.describe 'WeakQuizzes', type: :system do
       end
 
     visit start_weak_quizzes_path
-    expect(page).to have_content '苦手を克服したクイズの数: 10問'
+    expect(page).to have_content('苦手を克服したクイズの数:')
+    expect(page).to have_content('10問')
   end
 end
